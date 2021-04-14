d4d1aedcffec9538b487c9b48f98229574899896
HectorMontes10 <75736664+HectorMontes10@users.noreply.github.com>
2021-04-14 13:05:03 -0500

    Bug: Add if_exists='replace' in save function

--- data/process_data.py
+++ data/process_data.py
@@ -88,7 +88,7 @@ def save_data(df, database_filename):
     
     '''
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('Cleaned_messages', engine, index=False)
+    df.to_sql('Cleaned_messages', engine, if_exists='replace', index=False)
 
 def main():
     
